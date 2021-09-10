4cce4fe849173c021995aa743c7f97a2208c8be5
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-10 15:35:16 +0530

    Navbar Addition

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Navbar from './components/Navbar';
 import "bootstrap/dist/js/bootstrap.bundle";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <Navbar />
+     <Home />
     </div>
   );
 }
