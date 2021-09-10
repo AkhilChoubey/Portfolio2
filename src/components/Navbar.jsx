b73855dcabb1c83b59bc80e7a7c4207f8e59ee2e
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-10 13:48:32 +0530

    Navbar Addition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,21 +6,21 @@ const Navbar = () => {
     return <>
      <div className="container-fluid nav_bg">
         <div className="row">
-            <div className="col-10 " style={{marginTop: "1%" , width:"100%"}}>
+            <div className="col-10 " style={{width:"100%"}}>
 
            
-        <nav className='navbar navbar-expand-lg navbar-light bg-light' style={{borderRadius: "41px"}}>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light' style={{borderRadius: "41px", padding: '0'}}>
         <div className="container-fluid">
         <Router>
-           <Link to='/' className='navbar-brand'> RecipeBook </Link>
+           <Link to='/' className='navbar-brand' style={{marginRight: '35%',fontSize: '2rem'}}> RecipeBook </Link>
            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
            <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className='navbar-nav ml-auto mb-2 mb-lg-0'>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/login' activeclassName="menu-active" className="nav-link">Login</Link><span></span></li>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/signup' activeclassName="menu-active" className="nav-link">Sign Up</Link><span></span></li>
-                <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/contact' activeclassName="menu-active" className="nav-link">Contact</Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "2%", fontSize: '1.5rem'}}><Link to='/login' activeclassName="menu-active" className="nav-link">Projects</Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "30px", fontSize: '1.5rem'}}><Link to='/signup' activeclassName="menu-active" className="nav-link">Certificates</Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "30px", fontSize: '1.5rem'}}><Link to='/contact' activeclassName="menu-active" className="nav-link">Contact</Link><span></span></li>
                 {/* <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/about' activeclassName="menu-active" className="nav-link">About</Link><span></span></li> */}
             </ul>
             </div>
