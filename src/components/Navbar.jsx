b3d5c137142ec73edabb9138f12797937af03d99
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-11 16:08:13 +0530

    Skills Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,8 +35,9 @@ const Navbar = () => {
             <ul className='navbar-nav ml-auto mb-2 mb-lg-0'>
                 <li className='nav-item menuitem' style={{marginRight: "18%", fontSize: '2rem'}}><Link to='/projects' activeclassname="menu-active" style={ colorChange?{color: 'white'}:{}} className="nav-link">Projects</Link><span></span></li>
                 <li className='nav-item menuitem' style={{marginRight: "18%", fontSize: '2rem'}}><Link to='/certificates' activeclassname="menu-active" style={ colorChange?{color: 'white'}:{}} className="nav-link">Certificates</Link><span></span></li>
+                <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/skills' activeclassName="menu-active" className="nav-link">Skills</Link><span></span></li>
                 <li className='nav-item menuitem' style={{fontSize: '2rem'}}><Link to='/contact' activeclassname="menu-active" style={ colorChange?{color: 'white'}:{}} className="nav-link">Contact</Link><span></span></li>
-                {/* <li className='nav-item menuitem' style={{marginRight: "30px"}}><Link to='/about' activeclassName="menu-active" className="nav-link">About</Link><span></span></li> */}
+                
             </ul>
             </div>
             {/* <span className="slide"></span> */}
