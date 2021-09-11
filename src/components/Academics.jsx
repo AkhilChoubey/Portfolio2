56ed8c4627d6ffd290a8388d9b98cdf19624cda0
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-11 23:40:34 +0530

    Skills Component

---
+++ src/components/Academics.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+
+const Academics = () => {
+    return <>
+        <div>
+            <Card key='1' title='10th' time='2016-17' institute='ABPS, Rehla' value='90'/>
+            <Card key='2' title='12th' time='2017-19' institute='ABPS, Rehla' value='86.2'/>
+            <Card key='3' title='Graduation' time='2019-23' institute='Lovely Professional University' value='87.9'/>
+        </div>
+    </>
+}
+
+export default Academics;
