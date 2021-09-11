70da21c5d57cda10082047d4358e23d2904e8832
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-11 23:48:36 +0530

    Academics Component

--- src/components/Academics.jsx
+++ src/components/Academics.jsx
@@ -4,7 +4,9 @@ import Card from './Card';
 
 const Academics = () => {
     return <>
-        <div>
+    <h1 style={{textAlign: 'center', fontWeight: 'bolder', fontSize: '4rem'}}>Academics</h1>
+        <div style={{display:'flex'}}>
+            
             <Card key='1' title='10th' time='2016-17' institute='ABPS, Rehla' value='90'/>
             <Card key='2' title='12th' time='2017-19' institute='ABPS, Rehla' value='86.2'/>
             <Card key='3' title='Graduation' time='2019-23' institute='Lovely Professional University' value='87.9'/>
