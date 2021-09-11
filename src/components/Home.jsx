eee3b1d40383432249b2b5d92ae70c4d4bcfb21d
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-11 10:01:10 +0530

    Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import newbg from './profile.jpg';
 import { Link } from 'react-router-dom';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import Typewriter from 'typewriter-effect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import bg from '../background/new.gif';
-// import bg1 from '../background/new2.gif';
-// import bg2 from '../background/new3.gif';
-// import bg3 from '../background/new4.gif';
-// import bg4 from '../background/new5.gif';
-// import bg5 from '../background/new6.gif';
-// import bg6 from '../background/new7.gif';
-//import bg7 from '../background/new8.gif';
+
 
 const Home = () => {
+
+
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
 
 return <div style ={{ background : `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${bg})`, backgroundAttachment: "fixed",backgroundRepeat: "no-repeat", backgroundSize: 'cover',height: '32rem'}} >
     
     {/* <h1> {props.location.state.details.first_name}</h1> */}
-    <section id="header" className="d-flex align-items-center" style={{marginTop: '5.1%',paddingTop: '75px'}} >
+    <section id="header" className="d-flex align-items-center" style={{marginTop: '5.1%',paddingTop: '75px'}} data-aos='zoom-in'  >
     <div id="title" className="titleHeader col-md-6 pt-5 pt-lg-0 order-2 order-lg- d-flex justify-content-center flex-column" >
                 <h1>Hello, I am Akhil Kr Choubey <strong className="brand-name"> 
                 <Typewriter options={{  strings: ["I am a Web Developer", "I am a Programmer", "I am a Debugger"],  autoStart: true,  loop: true,changeDelay:2 }}/>
