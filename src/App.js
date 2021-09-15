0eb7309d2461f727cd6797b0bf891b7b6d1d63ab
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-15 15:46:19 +0530

    Projects Component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route , Link} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import "bootstrap/dist/js/bootstrap.bundle";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-//import "./bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./bootstrap.min.css";
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
