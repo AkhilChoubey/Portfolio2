5fdfdcefcd035cbd987c6b91e11ad69d522d19ca
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-17 13:00:37 +0530

    Certificates

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,11 +1,29 @@
-import React,{ useState, useCallback } from 'react';
-import ImageGallery from 'react-image-gallery';
+import React,{ useState, useCallback,useEffect } from 'react';
+
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import dreamclues from './images/DreamsClues.png';
 import BigData from './images/BigData.png';
 import GG from './images/GitandGithub.jpg';
+import Python from './images/Python.png';
+import AnimatedCursor from "react-animated-cursor";
+
 const Certificates = () => {
+
+  <AnimatedCursor
+  innerSize={8}
+  outerSize={8}
+  color='255,255,255'
+  outerAlpha={0.5}
+  innerScale={0.9}
+  outerScale={10}
+/>
+
+useEffect(() => {
+  Aos.init({duration: 2000});
+}, []);
 
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
@@ -19,50 +37,49 @@ const Certificates = () => {
     setCurrentImage(0);
     setViewerIsOpen(false);
   };
-    const images = [
-        {
-          original: dreamclues,
-          thumbnail: dreamclues//'https://picsum.photos/id/1018/250/150/',
-        },
-        {
-          original: 'https://picsum.photos/id/1015/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1015/250/150/',
-        },
-        {
-          original: 'https://picsum.photos/id/1019/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1019/250/150/',
-        },
-      ];
+
       const photos = [
         {
           src: dreamclues, 
           // "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-          width: 420,
-          height: 200
+          width: 10,
+          height: 4
         },
         {
           src: BigData,
-          width: 1,
-          height: 1
+          width: 5,
+          height:3
         },
         {
           src: GG,
-          width: 3,
-          height: 4
+          width: 5,
+          height: 3
         },
         {
-          src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-          width: 3,
-          height: 4
+          src: "https://udemy-certificate.s3.amazonaws.com/image/UC-e257929e-3256-4e0b-b7a4-4e39d68f62f7.jpg",
+          width: 5,
+          height: 3
+        },
+        {
+          src: Python,
+          width: 5,
+          height: 3
+        },
+        {
+          src: "https://udemy-certificate.s3.amazonaws.com/image/UC-8KXCZXSB.jpg",
+          width: 5,
+          height: 3
+        },
+        {
+          src: "https://udemy-certificate.s3.amazonaws.com/image/UC-JQR18PFF.jpg",
+          width: 5,
+          height: 3
         },
        
       ];
       
     return <>
-        {/* return 
-        <Gallery photos={photos} />; */}
-          {/* <ImageGallery items={images} />; */}
-          <div>
+          <div data-aos='zoom-in'>
       <Gallery photos={photos} onClick={openLightbox} />
       <ModalGateway>
         {viewerIsOpen ? (
