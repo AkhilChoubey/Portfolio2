fbe799c8c00d620c3018c522faabd0b168931f0a
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-17 12:36:38 +0530

    Certificates

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React,{ useState, useCallback } from 'react';
 import ImageGallery from 'react-image-gallery';
-
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import dreamclues from './images/DreamsClues.png';
+import BigData from './images/BigData.png';
+import GG from './images/GitandGithub.jpg';
 const Certificates = () => {
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
     const images = [
         {
-          original: 'https://picsum.photos/id/1018/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1018/250/150/',
+          original: dreamclues,
+          thumbnail: dreamclues//'https://picsum.photos/id/1018/250/150/',
         },
         {
           original: 'https://picsum.photos/id/1015/1000/600/',
@@ -16,9 +33,52 @@ const Certificates = () => {
           thumbnail: 'https://picsum.photos/id/1019/250/150/',
         },
       ];
+      const photos = [
+        {
+          src: dreamclues, 
+          // "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+          width: 420,
+          height: 200
+        },
+        {
+          src: BigData,
+          width: 1,
+          height: 1
+        },
+        {
+          src: GG,
+          width: 3,
+          height: 4
+        },
+        {
+          src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+          width: 3,
+          height: 4
+        },
+       
+      ];
       
     return <>
-        return <ImageGallery items={images} />;
+        {/* return 
+        <Gallery photos={photos} />; */}
+          {/* <ImageGallery items={images} />; */}
+          <div>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
 
     </>
 }
