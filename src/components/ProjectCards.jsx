b762f28f88509d1a38150645dd9c8641904f162e
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-27 15:10:55 +0530

    Working on project component

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Project.css';
-import img from './profile.jpg';
 import {BsBoxArrowUpRight} from 'react-icons/bs';
 
 const ProjectCards = (props) => {
     return <>
         <div className="col-md-4 col-10 mx-auto" >
-        <div className="container1">
+        <div>
 	        <div className="box">
 		        <div className="imgBx">
-			        <img src={img} /> 
+			        <img src={props.image} /> 
                     {/* <img src={props.image} /> */}
 		        </div>
 		        <div className="content"><a href={props.link} style={{textDecoration:'none'}}><h2>{props.title} <BsBoxArrowUpRight/> </h2> </a> </div>
