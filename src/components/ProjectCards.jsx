7ad3a74ef274573e4628712d6c044cd7bd9440be
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-27 14:28:35 +0530

    Working on project component

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Project.css';
 import img from './profile.jpg';
-const ProjectCards = () => {
+import {BsBoxArrowUpRight} from 'react-icons/bs';
+
+const ProjectCards = (props) => {
     return <>
         <div className="col-md-4 col-10 mx-auto" >
         <div className="container1">
 	        <div className="box">
 		        <div className="imgBx">
-			        <img src={img} />
+			        <img src={img} /> 
+                    {/* <img src={props.image} /> */}
 		        </div>
-		        <div className="content"><h2>New Project</h2> </div>
+		        <div className="content"><a href={props.link} style={{textDecoration:'none'}}><h2>{props.title} <BsBoxArrowUpRight/> </h2> </a> </div>
 	        </div>
         </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,12 +8,14 @@ const Projects = () => {
             <div className="row" >
                 <div className="col-10 top-elements" style={{marginTop: '3rem', width: '100%'}} >
                     <div className="row gy-4">  
-        <ProjectCards />
-        <ProjectCards />
-        <ProjectCards />
-        <ProjectCards />
-        <ProjectCards />
-        <ProjectCards />
+        <ProjectCards key='1' title='Weather App' image='' link='https://akhilweather.herokuapp.com/' />
+        <ProjectCards key='2' title='warta chatting app' image='' link='https://akhil-warta.netlify.app/' />
+        <ProjectCards key='3' title='recipebook' image='' link='https://akhilrecipeapp.netlify.app/' />
+        <ProjectCards key='4' title='portfolio' image='' link='https://akhilportfolio007.netlify.app/' />
+        <ProjectCards key='5' title='playmusic' image='' link='https://akhilchoubey.github.io/playMusic/' />
+        <ProjectCards key='6' title='to-do-list' image='' link='https://github.com/AkhilChoubey/To-Do-List' />
+        <ProjectCards key='7' title='Indian TOURISM' image='' link='https://akhilchoubey.github.io/India-Tourism/' />
+        <ProjectCards key='8' title='IMAGE RESIZER' image='' link='https://akhilchoubey.github.io/imageResize/' />
          </div>
         </div>
         </div>
