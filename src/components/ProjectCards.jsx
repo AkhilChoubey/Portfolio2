649079616b67ecff954ed3e5cbcd0db344dda230
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-27 15:14:05 +0530

    Working on project component

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 import './Project.css';
 import {BsBoxArrowUpRight} from 'react-icons/bs';
 
 const ProjectCards = (props) => {
+
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
+
+
     return <>
-        <div className="col-md-4 col-10 mx-auto" >
+        <div className="col-md-4 col-10 mx-auto" data-aos='zoom-in' >
         <div>
 	        <div className="box">
 		        <div className="imgBx">
