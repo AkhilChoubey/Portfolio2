6929b04112fde65a52d49ee276271b2fb8b6a5eb
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-27 11:59:24 +0530

    Working on project component

---
+++ src/components/ProjectCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Project.css';
+const ProjectCards = () => {
+    return <>
+        <div class="container">
+	        <div class="box">
+		        <div class="imgBx">
+			        <img src="images/taj.jpg" />
+		        </div>
+		        <div class="content"><h2>Taj Mahal</h2> </div>
+	        </div>
+        </div>
+    </>
+}
