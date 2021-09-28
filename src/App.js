f9cde32ba99d5365b3082522ac473de7e59ef2b2
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-09-29 00:52:43 +0530

    Final touch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import "./bootstrap.min.css";
+
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
